7bf2baf95bfd18a1706c491f66a456e12685e796
Rebecca Collins <hello@rebeccacollins.me>
2014-04-06 13:53:51 -0400

    shiny new dashboard

---
+++ jobs/sample.rb
@@ -0,0 +1,16 @@
+current_valuation = 0
+current_karma = 0
+
+SCHEDULER.every '2s' do
+  last_valuation = current_valuation
+  last_karma     = current_karma
+  current_valuation = rand(100)
+  current_karma     = rand(200000)
+  t  = Time.now
+  the_time = t + 18000
+
+  send_event('welcome', { title: "BOO!", text: "It's #{the_time.strftime "%H:%M"}" })
+  send_event('valuation', { current: current_valuation, last: last_valuation })
+  send_event('karma', { current: current_karma, last: last_karma })
+  send_event('synergy',   { value: rand(50) })
+end
