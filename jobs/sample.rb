cc1400878eea02c785eb0b79cc8de707320c409a
Rebecca Collins <hello@rebeccacollins.me>
2014-04-13 05:07:30 -0400

    adds gitcommit widget

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -7,10 +7,11 @@ SCHEDULER.every '2s' do
   current_valuation = rand(100)
   current_karma     = rand(200000)
   t  = Time.now
-  the_time = t + 18000
+  the_time = t + 21600
 
   send_event('welcome', { title: "BOO!", text: "It's #{the_time.strftime "%H:%M"}" })
   send_event('valuation', { current: current_valuation, last: last_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
   send_event('synergy',   { value: rand(50) })
+  send_event('recent_git_commit', { text: "one step closer to a git commit message" })
 end
